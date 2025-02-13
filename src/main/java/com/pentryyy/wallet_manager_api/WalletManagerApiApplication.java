568f199f21fa9eeb722421934240996605e85be3
pentryyy <m.v.volikov@gmail.com>
2025-02-13 16:19:12 +0300

    Добавлена основа проекта

---
+++ src/main/java/com/pentryyy/wallet_manager_api/WalletManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pentryyy.wallet_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pentryyy/wallet_manager_api/WalletManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pentryyy.wallet_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
